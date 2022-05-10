dbbc78b86b3683aaab0a3ef9048965a4fdf089c9
Pierre667G <pgerard@edenschool.fr>
2022-05-10 17:12:12 +0200

    finish progress bar and add background and img folder

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
-let div = document.getElementById('progress');
-let progress = document.getElementById('progressing')
-for (let i = 0; i < div.length; i++) {
-    
-    
+const mel = document.getElementById('progressing');
+let counter = 0;
+let random = Math.floor(Math.random() * (15-3) + 3);
+function progression(a) {
+    counter = counter + a;
+    mel.style.width = `${counter}px`;
 }
+setInterval(() => progression(0.1), random);
